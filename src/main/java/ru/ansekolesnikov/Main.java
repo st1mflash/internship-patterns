a70716752608e026f4f3ecbff425da4fa8462973
ansekolesnikov <st1mflash@gmail.com>
2024-04-25 14:52:43 +0300

    add builder pattern

--- src/main/java/ru/ansekolesnikov/Main.java
+++ src/main/java/ru/ansekolesnikov/Main.java
@@ -2,6 +2,8 @@ package ru.ansekolesnikov;
 
 import ru.ansekolesnikov.model.application.Application;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
@@ -12,5 +14,17 @@ public class Main {
         scanner.close();
 
         application.sendForApproval();
+
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.DAY_OF_YEAR, 2);
+
+        new Application.ApplicationBuilder(
+                "Java",
+                "developer",
+                calendar.getTime(),
+                15000
+        );
     }
 }
--- src/main/java/ru/ansekolesnikov/model/application/Application.java
+++ src/main/java/ru/ansekolesnikov/model/application/Application.java
@@ -3,16 +3,100 @@ package ru.ansekolesnikov.model.application;
 import ru.ansekolesnikov.factory.ManagerFactory;
 import ru.ansekolesnikov.model.employee.Manager;
 
+import java.util.Date;
+
 public class Application {
-    private final int amount;
-    private final Manager manager;
-    public Application(int amount) {
-        this.amount = amount;
-        this.manager = new ManagerFactory().createManager(amount);
+    private String name;
+    private String client;
+    private Date creditTerm;
+    private int creditAmount;
+    private Manager manager;
+    private String description = "";
+    private String reason = "";
+
+    public Application(int creditAmount) {
+        this.creditAmount = creditAmount;
+        this.manager = new ManagerFactory().createManager(creditAmount);
     }
+
+    private Application(ApplicationBuilder applicationBuilder) {
+        this.name = applicationBuilder.name;
+        this.client = applicationBuilder.client;
+        this.creditTerm = applicationBuilder.creditTerm;
+        this.creditAmount = applicationBuilder.creditAmount;
+        this.manager = applicationBuilder.manager;
+        this.description = applicationBuilder.description;
+        this.reason = applicationBuilder.reason;
+    }
+
     public void sendForApproval() {
         manager.verificationApplication(this);
         manager.approveApplication(this);
         manager.signingApplication(this);
+    }
+
+    public ApplicationBuilder builder() {
+        return null;
+    }
+
+    public static class ApplicationBuilder {
+        private String name;
+        private String client;
+        private Date creditTerm;
+        private int creditAmount;
+        private Manager manager = null;
+        private String description = "";
+        private String reason = "";
+
+        public ApplicationBuilder(
+                String name,
+                String client,
+                Date creditTerm,
+                int creditAmount
+        ) {
+            this.name = name;
+            this.client = client;
+            this.creditTerm = creditTerm;
+            this.creditAmount = creditAmount;
+        }
+
+        public ApplicationBuilder name(String val) {
+            this.name = val;
+            return this;
+        }
+
+        public ApplicationBuilder client(String val) {
+            this.client = val;
+            return this;
+        }
+
+        public ApplicationBuilder creditTerm(Date val) {
+            this.creditTerm = val;
+            return this;
+        }
+
+        public ApplicationBuilder creditAmount(int val) {
+            this.creditAmount = val;
+            return this;
+        }
+
+        public ApplicationBuilder manager(Manager val) {
+            this.manager = val;
+            return this;
+        }
+
+        public ApplicationBuilder description(String val) {
+            this.description = val;
+            return this;
+        }
+
+        public ApplicationBuilder reason(String val) {
+            this.reason = val;
+            return this;
+        }
+
+        public Application build() {
+            return new Application(this);
+        }
     }
 }
